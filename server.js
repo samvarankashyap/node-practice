0fd23d0a4f66a907bc4272be1a3f1fa05cb01830
samvaran kashyap <samvaran.kashyap@gmail.com>
2014-12-07 21:37:21 +0530

    refractored server.js code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
-http.createServer(function (req, res) {
+var handler = function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337);
+  res.end('Hello nod.js i love you\n');
+}
+server = http.createServer(handler)
+server.listen(1337);
 console.log('Server running at http://127.0.0.1:1337/');
